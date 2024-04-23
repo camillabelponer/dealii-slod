be42f2ed7f8d09fc46b73fbcdd45d4d27589fecb
Matthias Deiml <matthias.deiml@uni-a.de>
2024-04-23 14:30:52 +0200

    Rename functions in test to mathematically accurate name

--- tests/projection_01.cc
+++ tests/projection_01.cc
@@ -39,7 +39,7 @@ test()
   FullMatrix<Number> projection_matrix(fe_coarse.n_dofs_per_cell(), fe_fine.n_dofs_per_cell());
   FETools::get_projection_matrix(fe_fine, fe_coarse, projection_matrix);
 
-  // avereging
+  // averaging (inverse of P0 mass matrix)
   VectorType valence_coarse(dof_handler_coarse.n_dofs());
   VectorType local_identity_coarse(fe_coarse.n_dofs_per_cell());
   local_identity_coarse = 1.0;
@@ -47,11 +47,11 @@ test()
   for (const auto &cell : dof_handler_coarse.active_cell_iterators())
     cell->distribute_local_to_global(local_identity_coarse, valence_coarse);
 
-  for (auto &i : valence_coarse)
-    i = 1.0 / i;
+  for (auto &elem : valence_coarse)
+    elem = 1.0 / elem;
 
   // define interapolation function and its transposed
-  const auto interpolate = [&](auto &dst, const auto &src) {
+  const auto projectT = [&](auto &dst, const auto &src) {
     VectorType vec_local_coarse(fe_coarse.n_dofs_per_cell());
     VectorType vec_local_fine(fe_fine.n_dofs_per_cell());
     VectorType weights(fe_coarse.n_dofs_per_cell());
@@ -73,7 +73,7 @@ test()
       }
   };
 
-  const auto interpolateT = [&](auto &dst, const auto &src) {
+  const auto project = [&](auto &dst, const auto &src) {
     VectorType vec_local_coarse(fe_coarse.n_dofs_per_cell());
     VectorType vec_local_fine(fe_fine.n_dofs_per_cell());
     VectorType weights(fe_coarse.n_dofs_per_cell());
@@ -99,8 +99,8 @@ test()
   VectorType vec_coarse(dof_handler_coarse.n_dofs());
   VectorType vec_fine(dof_handler_fine.n_dofs());
 
-  interpolate(vec_coarse, vec_fine);
-  interpolateT(vec_fine, vec_coarse);
+  projectT(vec_coarse, vec_fine);
+  project(vec_fine, vec_coarse);
 }
 
 void
@@ -147,5 +147,7 @@ main(int argc, char *argv[])
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
   test(); // manually
 
+  // THIS DOES NOT DO WHAT IT SHOULD
+  // (the projection goes the wrong way)
   test_mg(); // with MG infrastructure
 }
