c8cb2b0eb65787e42f52a3099cd94a6e22199360
Peter Munch <peterrmuench@gmail.com>
2024-07-22 17:39:44 +0200

    Continue

--- tests/assembly_02.cc
+++ tests/assembly_02.cc
@@ -1,3 +1,5 @@
+#include <deal.II/distributed/shared_tria.h>
+
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
 
@@ -24,18 +26,16 @@ main(int argc, char **argv)
 {
   Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
 
-  // 0) parameters
   const unsigned int dim       = 2;
-  const unsigned int fe_degree = 7;
-  const unsigned int n_overlap = 3; // numbers::invalid_unsigned_int
+  const unsigned int fe_degree = 1;
+  const unsigned int n_overlap = 1; // numbers::invalid_unsigned_int
   const MPI_Comm     comm      = MPI_COMM_WORLD;
 
-  std::vector<unsigned int> repetitions = {{10, 10}};
+  std::vector<unsigned int> repetitions = {{5, 5}};
   Point<dim>                p1(0, 0);
   Point<dim>                p2(1, 1);
 
-  // 2) create mesh
-  Triangulation<dim> tria;
+  parallel::shared::Triangulation<dim> tria(comm);
   GridGenerator::subdivided_hyper_rectangle(tria, repetitions, p1, p2);
 
   types::global_dof_index n_dofs_coarse = 1;
@@ -54,7 +54,6 @@ main(int argc, char **argv)
       Utilities::MPI::n_mpi_processes(comm),
       n_dofs_fine);
 
-  // 3) create patch
   Patch<dim> patch(fe_degree, repetitions);
 
   IndexSet locally_owned_cells(n_dofs_coarse);
@@ -63,6 +62,7 @@ main(int argc, char **argv)
     if (cell->is_locally_owned())
       locally_owned_cells.add_index(cell->active_cell_index());
 
+  // 2) ininitialize sparsity pattern
   TrilinosWrappers::SparsityPattern sparsity_pattern_A_lod(locally_owned_cells,
                                                            comm);
 
@@ -106,9 +106,11 @@ main(int argc, char **argv)
   sparsity_pattern_A_lod.compress();
   sparsity_pattern_C.compress();
 
+  // 3) initialize matrices
   TrilinosWrappers::SparseMatrix A_lod(sparsity_pattern_A_lod);
   TrilinosWrappers::SparseMatrix C(sparsity_pattern_C);
 
+  // 4) set dummy constraints (TODO: adjust for LOD)
   for (const auto &cell : tria.active_cell_iterators())
     if (cell->is_locally_owned()) // parallel for-loop
       {
@@ -132,4 +134,84 @@ main(int argc, char **argv)
       }
 
   C.compress(VectorOperation::values::insert);
+
+  // 5) convert sparse matrix C to shifted AffineConstraints
+  IndexSet constraints_lod_fem_locally_owned_dofs(n_dofs_fine + n_dofs_coarse);
+  constraints_lod_fem_locally_owned_dofs.add_indices(locally_owned_cells);
+  constraints_lod_fem_locally_owned_dofs.add_indices(locally_owned_fine_dofs,
+                                                     n_dofs_coarse);
+
+  IndexSet constraints_lod_fem_locally_stored_constraints =
+    constraints_lod_fem_locally_owned_dofs;
+
+  for (const auto row : locally_owned_fine_dofs) // parallel for-loop
+    {
+      for (auto entry = C.begin(row); entry != C.end(row); ++entry)
+        constraints_lod_fem_locally_stored_constraints.add_index(
+          entry->column()); // coarse
+    }
+
+  for (const auto &cell : tria.active_cell_iterators())
+    if (cell->is_locally_owned()) // parallel for-loop
+      {
+        patch.reinit(cell, n_overlap);
+
+        const auto                           n_dofs_patch = patch.n_dofs();
+        std::vector<types::global_dof_index> local_dof_indices_fine(
+          n_dofs_patch);
+        patch.get_dof_indices(local_dof_indices_fine);
+
+        for (unsigned int i = 0; i < n_dofs_patch; ++i)
+          constraints_lod_fem_locally_stored_constraints.add_index(
+            local_dof_indices_fine[i] + n_dofs_coarse); // fine
+      }
+
+  AffineConstraints<double> constraints_lod_fem(
+    constraints_lod_fem_locally_owned_dofs,
+    constraints_lod_fem_locally_stored_constraints);
+  for (const auto row : locally_owned_fine_dofs) // parallel for-loop
+    {
+      std::vector<std::pair<types::global_dof_index, double>> dependencies;
+
+      for (auto entry = C.begin(row); entry != C.end(row); ++entry)
+        dependencies.emplace_back(entry->column(), entry->value());
+
+      if (true || !dependencies.empty())
+        constraints_lod_fem.add_constraint(row + n_dofs_coarse, dependencies);
+    }
+
+  constraints_lod_fem.make_consistent_in_parallel(
+    constraints_lod_fem_locally_owned_dofs,
+    constraints_lod_fem_locally_stored_constraints,
+    comm);
+  constraints_lod_fem.close();
+
+  // 6) assembly LOD matrix
+  for (const auto &cell : tria.active_cell_iterators())
+    if (cell->is_locally_owned()) // parallel for-loop
+      {
+        Patch<dim> patch(fe_degree, repetitions);
+        patch.reinit(cell, 1);
+
+        const unsigned int n_dofs_per_cell = patch.n_dofs();
+
+        // a) compute FEM element stiffness matrix
+        FullMatrix<double> cell_matrix_fem(n_dofs_per_cell, n_dofs_per_cell);
+
+        for (unsigned int i = 0; i < cell_matrix_fem.m(); ++i)
+          for (unsigned int j = 0; j < cell_matrix_fem.n(); ++j)
+            cell_matrix_fem[i][j] = 1.0; // TODO: element stiffeness matrix
+
+        // b) assemble into LOD matrix by using constraints
+        std::vector<types::global_dof_index> local_dof_indices(n_dofs_per_cell);
+        patch.get_dof_indices(local_dof_indices);
+
+        for (auto &i : local_dof_indices)
+          i += n_dofs_coarse; // shifted view
+
+        constraints_lod_fem.distribute_local_to_global(cell_matrix_fem,
+                                                       local_dof_indices,
+                                                       local_dof_indices,
+                                                       A_lod);
+      }
 }
