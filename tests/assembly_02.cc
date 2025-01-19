40dcfc4ae5195fc82abb0519a2741395250eb0c2
Peter Munch <peterrmuench@gmail.com>
2025-01-19 08:07:56 +0100

    Loop over subcells

--- tests/assembly_02.cc
+++ tests/assembly_02.cc
@@ -825,18 +825,50 @@ main(int argc, char **argv)
       // diffusion
       params.n_components = 1;
 
-      assemble_element_stiffness_matrix = [](const FEValues<dim> &fe_values,
-                                             FullMatrix<double>  &cell_matrix) {
-        const double alpha_value = 1.0; // TODO
+      const auto lexicographic_to_hierarchic_numbering =
+        FETools::lexicographic_to_hierarchic_numbering<dim>(
+          params.n_subdivisions_fine);
 
-        for (const unsigned int q_index : fe_values.quadrature_point_indices())
-          for (const unsigned int i : fe_values.dof_indices())
-            for (const unsigned int j : fe_values.dof_indices())
-              cell_matrix(i, j) +=
-                alpha_value *
-                (fe_values.shape_grad(i, q_index) *
-                 fe_values.shape_grad(j, q_index) * fe_values.JxW(q_index));
-      };
+      assemble_element_stiffness_matrix =
+        [&params, lexicographic_to_hierarchic_numbering](
+          const FEValues<dim> &fe_values, FullMatrix<double> &cell_matrix) {
+          const double alpha_value = 1.0; // TODO
+
+          for (unsigned int c_1 = 0; c_1 < params.n_subdivisions_fine; ++c_1)
+            for (unsigned int c_0 = 0; c_0 < params.n_subdivisions_fine; ++c_0)
+              for (unsigned int q_1 = 0; q_1 < 2; ++q_1)
+                for (unsigned int q_0 = 0; q_0 < 2; ++q_0)
+                  {
+                    const unsigned int q_index =
+                      (c_0 * 2 + q_0) +
+                      (c_1 * 2 + q_1) * (2 * params.n_subdivisions_fine);
+
+                    for (unsigned int i_1 = 0; i_1 < 2; ++i_1)
+                      for (unsigned int i_0 = 0; i_0 < 2; ++i_0)
+                        {
+                          const unsigned int i =
+                            lexicographic_to_hierarchic_numbering
+                              [(c_0 + i_0) +
+                               (c_1 + i_1) * (params.n_subdivisions_fine + 1)];
+
+                          for (unsigned int j_1 = 0; j_1 < 2; ++j_1)
+                            for (unsigned int j_0 = 0; j_0 < 2; ++j_0)
+                              {
+                                const unsigned int j =
+                                  lexicographic_to_hierarchic_numbering
+                                    [(c_0 + j_0) +
+                                     (c_1 + j_1) *
+                                       (params.n_subdivisions_fine + 1)];
+
+                                cell_matrix(i, j) +=
+                                  alpha_value *
+                                  (fe_values.shape_grad(i, q_index) *
+                                   fe_values.shape_grad(j, q_index) *
+                                   fe_values.JxW(q_index));
+                              }
+                        }
+                  }
+        };
     }
   else
     {
