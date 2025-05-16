93d65756fc214902ff570d1b3bf6a864c598220c
Peter Munch <peterrmuench@gmail.com>
2025-05-16 06:31:38 +0200

    Test vector diffusion

--- tests/assembly_02.cc
+++ tests/assembly_02.cc
@@ -51,7 +51,7 @@ namespace Step96
   struct Parameters
   {
     std::string  physics               = "elasticity";
-    unsigned int n_subdivisions_fine   = 3;
+    unsigned int n_subdivisions_fine   = 4;
     unsigned int n_components          = 1;
     unsigned int n_oversampling        = 2;
     unsigned int n_subdivisions_coarse = 8;
@@ -946,6 +946,68 @@ main(int argc, char **argv)
                                    fe_values.JxW(q_index));
                               }
                         }
+                  }
+        };
+    }
+  else if (params.physics == "diffusion-vector")
+    {
+      params.n_components = dim;
+
+      const auto lexicographic_to_hierarchic_numbering =
+        FETools::lexicographic_to_hierarchic_numbering<dim>(
+          params.n_subdivisions_fine);
+
+      assemble_element_stiffness_matrix =
+        [&params, lexicographic_to_hierarchic_numbering](
+          const FEValues<dim> &fe_values, FullMatrix<double> &cell_matrix) {
+          const double alpha_value = 1.0; // TODO: random variable
+
+          // TODO: better solution?
+          FESystem<dim> fe(FE_Q_iso_Q1<dim>(params.n_subdivisions_fine), dim);
+
+          const FEValuesExtractors::Vector displacement(0);
+
+          for (unsigned int c_1 = 0; c_1 < params.n_subdivisions_fine; ++c_1)
+            for (unsigned int c_0 = 0; c_0 < params.n_subdivisions_fine; ++c_0)
+
+              for (unsigned int q_1 = 0; q_1 < 2; ++q_1)
+                for (unsigned int q_0 = 0; q_0 < 2; ++q_0)
+                  {
+                    const unsigned int q =
+                      (c_0 * 2 + q_0) +
+                      (c_1 * 2 + q_1) * (2 * params.n_subdivisions_fine);
+                    for (unsigned int d_0 = 0; d_0 < 2; ++d_0)
+                      for (unsigned int i_1 = 0; i_1 < 2; ++i_1)
+                        for (unsigned int i_0 = 0; i_0 < 2; ++i_0)
+                          {
+                            const unsigned int i = fe.component_to_system_index(
+                              d_0,
+                              lexicographic_to_hierarchic_numbering
+                                [(c_0 + i_0) +
+                                 (c_1 + i_1) *
+                                   (params.n_subdivisions_fine + 1)]);
+
+                            for (unsigned int d_1 = 0; d_1 < 2; ++d_1)
+                              for (unsigned int j_1 = 0; j_1 < 2; ++j_1)
+                                for (unsigned int j_0 = 0; j_0 < 2; ++j_0)
+                                  {
+                                    const unsigned int j =
+                                      fe.component_to_system_index(
+                                        d_1,
+                                        lexicographic_to_hierarchic_numbering
+                                          [(c_0 + j_0) +
+                                           (c_1 + j_1) *
+                                             (params.n_subdivisions_fine + 1)]);
+
+                                if(d_0 == d_1)
+                                cell_matrix(i, j) +=
+                                  alpha_value * (1 + d_0) *
+                                  (fe_values.shape_grad(i, q) *
+                                   fe_values.shape_grad(j, q) *
+                                   fe_values.JxW(q));
+
+                                  }
+                          }
                   }
         };
     }
